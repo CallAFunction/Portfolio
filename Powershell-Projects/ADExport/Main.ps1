671fc30e22a80d176805ef008a5c647cd251e5c8
Nori <norishimogawa@gmail.com>
2025-12-20 21:26:19 +0000

    moved functions to functions.psm1

--- Powershell-Projects/ADExport/Main.ps1
+++ Powershell-Projects/ADExport/Main.ps1
@@ -1,35 +1,4 @@
-﻿#Import-Module "$PSScriptRoot\Modules\*"
-#while ($true) {
-
-
-function Test-PathSafe {
-    param([string]$Path)
-    if (!$path) { return $false }
-    return Test-Path $Path
-}
-
-function Import-Path {
-    Set-Content -Path "$PSScriptRoot\path.txt" -Value $Path
-}
-
-function ask-for-properties {    
-
-    $like = read-host "would you like to use all properties? (y/n)"
-    if ($like -match '^(y|yes)$') { 
-        return $all_properties  
-    }
-    elseif($like -match "^(n|no)"){
-        $selected_properties = @()
-     while(!($selected_properties -contains "Finished")){
-         $sel = $all_properties | Out-GridView -PassThru -Title "Select a property (click Finished when done)"
-            if ($sel -contains "Finished") { break }
-            $selected_properties += $sel
-        }
-        return $selected_properties
-        }
-
-    }
-
+﻿Import-Module "$PSScriptRoot\Modules\functions.psm1"
 
 # Prompt for path
 $path_reference = Get-Content "$PSScriptRoot\path.txt"
